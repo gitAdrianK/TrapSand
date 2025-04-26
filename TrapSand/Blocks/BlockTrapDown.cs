8e574a2080803158bb27580ed24d8407f8275a05
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2025-04-26 21:41:55 +0200

    Change blocks to inherit from BoxBlock

--- TrapSand/Blocks/BlockTrapDown.cs
+++ TrapSand/Blocks/BlockTrapDown.cs
@@ -3,28 +3,15 @@ namespace TrapSand.Blocks
     using JumpKing.Level;
     using Microsoft.Xna.Framework;
 
-    public class BlockTrapDown : IBlock, IBlockDebugColor
+    public class BlockTrapDown : BoxBlock, IBlockDebugColor
     {
         public static readonly Color BLOCKCODE_TRAP_DOWN = new Color(255, 68, 68);
 
-        private readonly Rectangle collider;
-
-        public BlockTrapDown(Rectangle collider) => this.collider = collider;
+        public BlockTrapDown(Rectangle collider) : base(collider) { }
 
         public Color DebugColor => BLOCKCODE_TRAP_DOWN;
 
-        public Rectangle GetRect() => this.collider;
-
-        public BlockCollisionType Intersects(Rectangle hitbox, out Rectangle intersection)
-        {
-            if (this.collider.Intersects(hitbox))
-            {
-                intersection = Rectangle.Intersect(hitbox, this.collider);
-                return BlockCollisionType.Collision_NonBlocking;
-            }
-            intersection = Rectangle.Empty;
-            return BlockCollisionType.NoCollision;
-        }
+        protected override bool canBlockPlayer => false;
     }
 }
 
--- TrapSand/Blocks/BlockTrapUp.cs
+++ TrapSand/Blocks/BlockTrapUp.cs
@@ -3,28 +3,15 @@ namespace TrapSand.Blocks
     using JumpKing.Level;
     using Microsoft.Xna.Framework;
 
-    public class BlockTrapUp : IBlock, IBlockDebugColor
+    public class BlockTrapUp : BoxBlock, IBlockDebugColor
     {
         public static readonly Color BLOCKCODE_TRAP_UP = new Color(255, 69, 69);
 
-        private readonly Rectangle collider;
-
-        public BlockTrapUp(Rectangle collider) => this.collider = collider;
+        public BlockTrapUp(Rectangle collider) : base(collider) { }
 
         public Color DebugColor => BLOCKCODE_TRAP_UP;
 
-        public Rectangle GetRect() => this.collider;
-
-        public BlockCollisionType Intersects(Rectangle hitbox, out Rectangle intersection)
-        {
-            if (this.collider.Intersects(hitbox))
-            {
-                intersection = Rectangle.Intersect(hitbox, this.collider);
-                return BlockCollisionType.Collision_NonBlocking;
-            }
-            intersection = Rectangle.Empty;
-            return BlockCollisionType.NoCollision;
-        }
+        protected override bool canBlockPlayer => false;
     }
 }
 
